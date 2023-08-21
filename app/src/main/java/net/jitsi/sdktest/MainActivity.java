ad18005017f8f5b77c6378785e2cf3109f98d4a1
DESKTOP-7U5ERIR\boyan.lin <boyan.lin@flowring.com>
2023-08-21 10:55:47 +0800

    翻譯英文註解

--- app/src/main/java/net/jitsi/sdktest/MainActivity.java
+++ app/src/main/java/net/jitsi/sdktest/MainActivity.java
@@ -36,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize default options for Jitsi Meet conferences.
+        // 初始化 Jitsi Meet 會議的默認選項。
         URL serverURL;
         try {
-            // When using JaaS, replace "https://meet.jit.si" with the proper serverURL
+            // 使用 JaaS 時，請將“https://meet.jit.si”替換為正確的 serverURL
             serverURL = new URL("https://meet.jit.si");
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -72,8 +72,8 @@ public class MainActivity extends AppCompatActivity {
         String text = editText.getText().toString();
 
         if (text.length() > 0) {
-            // Build options object for joining the conference. The SDK will merge the default
-            // one we set earlier and this one when joining.
+            // 構建用於加入會議的選項對象。 SDK會默認合併
+            // 一個是我們之前設置的，這個是加入時設置的。
             JitsiMeetConferenceOptions options
                     = new JitsiMeetConferenceOptions.Builder()
                     .setRoom(text)
@@ -81,8 +81,7 @@ public class MainActivity extends AppCompatActivity {
                     //.setAudioMuted(true)
                     //.setVideoMuted(true)
                     .build();
-            // Launch the new activity with the given options. The launch() method takes care
-            // of creating the required Intent and passing the options.
+            // 使用給定選項啟動新活動。 launch() 方法負責創建所需的 Intent 並傳遞選項。
             JitsiMeetActivity.launch(this, options);
         }
     }
@@ -90,12 +89,12 @@ public class MainActivity extends AppCompatActivity {
     private void registerForBroadcastMessages() {
         IntentFilter intentFilter = new IntentFilter();
 
-        /* This registers for every possible event sent from JitsiMeetSDK
-           If only some of the events are needed, the for loop can be replaced
-           with individual statements:
-           ex:  intentFilter.addAction(BroadcastEvent.Type.AUDIO_MUTED_CHANGED.getAction());
-                intentFilter.addAction(BroadcastEvent.Type.CONFERENCE_TERMINATED.getAction());
-                ... other events
+        /* 這會註冊從 JitsiMeetSDK 發送的每個可能的事件
+            如果只需要部分事件，可以替換for循環
+            附有個人陳述：
+            例如：intentFilter.addAction(BroadcastEvent.Type.AUDIO_MUTED_CHANGED.getAction());
+                 IntentFilter.addAction(BroadcastEvent.Type.CONFERENCE_TERMINATED.getAction());
+                 ...其他活動
          */
         for (BroadcastEvent.Type type : BroadcastEvent.Type.values()) {
             intentFilter.addAction(type.getAction());
@@ -104,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter);
     }
 
-    // Example for handling different JitsiMeetSDK events
+    // 處理不同 JitsiMeetSDK 事件的示例
     private void onBroadcastReceived(Intent intent) {
         if (intent != null) {
             BroadcastEvent event = new BroadcastEvent(intent);
@@ -120,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Example for sending actions to JitsiMeetSDK
+    // 向 JitsiMeetSDK 發送操作的示例
     private void hangUp() {
         Intent hangupBroadcastIntent = BroadcastIntentHelper.buildHangUpIntent();
         LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(hangupBroadcastIntent);
